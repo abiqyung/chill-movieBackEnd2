fefb4e9f0bb7d911d5d0be197b014bacfc48d4c6
abiqyung <abiqyung@gmail.com>
2024-10-01 23:57:58 +0700

    adding uploads

--- src/middleware/multerService.js
+++ src/middleware/multerService.js
@@ -1,34 +1,14 @@
 const multer = require("multer");
-const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../../uploads/");
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
-    );
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
-  if (allowedTypes.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    cb(
-      new Error("Invalid file type. Only JPEG, PNG, and GIF are allowed."),
-      false
-    );
-  }
-};
-
-const upload = multer({
-  storage: storage,
-  fileFilter: fileFilter,
-});
+const upload = multer({ storage: storage });
 
 module.exports = upload;
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,17 +1,35 @@
 const express = require("express");
 const MoviesController = require("../controller/movies.js");
 const authMiddleware = require("../middleware/authMiddleware");
+const multer = require("multer");
 
 const router = express.Router();
 
+// Set up multer storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // Define where to store the uploaded files
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname); // File naming convention
+  },
+});
+
+// Initialize upload middleware with storage
+const upload = multer({ storage: storage });
+
+// Routes
 router.post("/", authMiddleware, MoviesController.createNewMovie);
 router.get("/", MoviesController.getAllMovies);
 router.patch("/:movie_id", authMiddleware, MoviesController.updateMovie);
 router.delete("/:movie_id", authMiddleware, MoviesController.deleteMovie);
 router.get("/search", MoviesController.searchMovies);
+
+// Upload route with multer middleware
 router.post(
   "/upload",
-  upload.single("poster"),
+  upload.single("poster"), // Middleware to handle single file upload
   MoviesController.uploadMoviePoster
 );
+
 module.exports = router;
